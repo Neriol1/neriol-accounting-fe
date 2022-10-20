f86bf965211858e6d00e74cc3cf5288c83d7510f
Neriol <neriol@163.com>
2022-10-20 22:25:10 +0800

    使用tsx

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // base:'/neriol-accounting-fe/dist/', //部署到github时需要配置
-  plugins: [vue()]
+  plugins: [
+    vueJsx({
+      // options are passed on to @vue/babel-plugin-jsx
+      transformOn: true,
+      mergeProps: true
+    }),
+    vue()
+  ]
 })
