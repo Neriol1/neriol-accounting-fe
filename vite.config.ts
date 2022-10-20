50bfc81daa167c06e294ae42b7904c51b210134f
Neriol <neriol@163.com>
2022-10-20 20:59:10 +0800

    ignore dist

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/neriol-accounting-fe/dist/',
+  // base:'/neriol-accounting-fe/dist/', //部署到github时需要配置
   plugins: [vue()]
 })
