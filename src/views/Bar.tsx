594f0bb80f8a03b9e21a8f557ad9d6f978f64093
Neriol <neriol@163.com>
2022-10-20 23:06:19 +0800

    引入vueRouter 4

---
+++ src/views/Bar.tsx
@@ -0,0 +1,9 @@
+import { defineComponent, ref } from "vue"
+export const Bar = defineComponent({
+  setup: (props, context) => {
+    return () => 
+      <>
+        <div>Bar</div>
+      </>
+  },
+})
