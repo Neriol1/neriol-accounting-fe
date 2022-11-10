e8d35c8fb7fe1fdf4298e9b9dd8e9ffc9b6e67d1
Neriol <neriol@163.com>
2022-11-11 01:02:38 +0800

    封装charts

---
+++ src/components/statistics/Charts.tsx
@@ -0,0 +1,33 @@
+import { defineComponent, PropType, ref } from 'vue';
+import { FormItem } from '../../utils/Form';
+import { Bars } from './Bars';
+import s from './Charts.module.scss';
+import { LineChart } from './LineChart';
+import { PieChart } from './PieChart';
+export const Charts = defineComponent({
+  props: {
+    startDate: {
+      type: String as PropType<string>,
+      required: true
+    },
+    endDate: {
+      type: String as PropType<string>,
+      required: true
+    }
+  },
+  setup: (props, context) => {
+    const category = ref('expenses')
+    return () => (
+      <div class={s.wrapper}>
+         <FormItem label='类型' type="select" options={[
+          { value: 'expenses', label: '支出' },
+          { value: 'income', label: '收入' }
+        ]} v-model={category.value} />
+
+        <LineChart />
+        <PieChart />
+        <Bars />
+      </div>
+    )
+  }
+})
