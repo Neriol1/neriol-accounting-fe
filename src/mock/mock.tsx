165999d042182dd92f84c93eee8e082b0d060101
Neriol <neriol@163.com>
2022-12-25 13:21:38 +0000

    mock TagEdit

--- src/mock/mock.tsx
+++ src/mock/mock.tsx
@@ -7,6 +7,17 @@ export const mockSession: Mock = (config) => {
   return [200, { jwt: faker.random.word() }]
 }
 
+export const mockTagEdit: Mock = (config) => {
+  const createTag = (attrs?: any) => ({
+    id: createId(),
+    name: faker.lorem.word(),
+    sign: faker.internet.emoji(),
+    kind: config.params.kind,
+    ...attrs,
+  })
+  return [200, { resource: createTag() }]
+}
+
 export const mockTagShow: Mock = (config) => {
   const createTag = (attrs?: any) => ({
     id: createId(),
