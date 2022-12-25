430900db134516bf8e9f65c5e75ab94cd0069f2c
Neriol <neriol@163.com>
2022-12-25 13:19:10 +0000

    删除标签

--- src/components/tag/TagEdit.tsx
+++ src/components/tag/TagEdit.tsx
@@ -1,18 +1,33 @@
-import { defineComponent, reactive } from 'vue'
+import { defineComponent } from 'vue'
 import { MainLayout } from '../../layouts/MainLayout'
 import { Button } from '../../shared/Button'
 import s from './Tag.module.scss'
 import '../../shared/validate'
-import { Rules,validate } from '../../shared/validate'
 import { TagForm } from './TagForm'
 import { BackIcon } from '../../shared/BackIcon'
-import { useRoute } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
+import { Dialog } from 'vant'
+import { http } from '../../shared/Http'
 export const TagEdit = defineComponent({
   setup: (props, context) => {
     const route = useRoute()
+    const router = useRouter()
     const numberId = parseInt(route.params.id!.toString())
     if(isNaN(numberId)){
       return <div>id 不存在</div>
+    }
+    const onError = () => {
+      Dialog.alert({title:'提示',message:'删除失败'})
+    }
+    const onDelete = async (options?:{withItems?:boolean}) =>{
+      await Dialog.confirm({
+        title: '确认',
+        message: '确认删除吗？'
+      })
+      await http.delete(`/tags/${numberId}`,{
+        withItems:options?.withItems ? 'true' : 'false'
+      }).catch(onError)
+      router.back()
     }
     return () => (
       <>
@@ -24,8 +39,8 @@ export const TagEdit = defineComponent({
               <>
                 <TagForm id={numberId}></TagForm>
                 <div class={s.actions}>
-                  <Button level='danger' class={s.removeTags} onClick={() => { }}>删除标签</Button>
-                  <Button level='danger' class={s.removeTagsAndItems} onClick={() => { }}>删除标签和记账</Button>
+                  <Button level='danger' class={s.removeTags} onClick={() => onDelete}>删除标签</Button>
+                  <Button level='danger' class={s.removeTagsAndItems} onClick={() => onDelete({withItems:true})}>删除标签和记账</Button>
                 </div>
               </>
             ),
