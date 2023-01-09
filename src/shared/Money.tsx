fec1d00e607f5db865b125124e95b7735171b3cc
Neriol <neriol@163.com>
2023-01-09 17:06:17 +0800

    美化折线图

--- src/shared/Money.tsx
+++ src/shared/Money.tsx
@@ -7,17 +7,21 @@ export const Money = defineComponent({
     },
   },
   setup: (props, context) => {
-    const addZero = (n:number)=>{
-      const nString = n.toString()
-      const doIndex = nString.indexOf('.')
-      if(doIndex < -1){
-        return nString + '.00'
-      }else if(nString.substring(doIndex).length === 2){
-        return nString + '0'
-      }else{
-        return nString
-      }
-    }
     return () => <span>{addZero(props.value / 100)}</span>
   },
 })
+export const addZero = (n:number)=>{
+  const nString = n.toString()
+  const doIndex = nString.indexOf('.')
+  if(doIndex < -1){
+    return nString + '.00'
+  }else if(nString.substring(doIndex).length === 2){
+    return nString + '0'
+  }else{
+    return nString
+  }
+}
+
+export const getMoney = (n: number) => {
+  return addZero(n / 100)
+}
